ccee310aadc417f85f5f20e78cecede6ccbfdd63
adados <adado@boardreader.com>
2019-05-15 13:19:11 -0400

    first commit

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { AngularKarmaJasminePage } from './app.po';
 
-describe('angular-karma-jasmine App', () => {
+describe('Welcome to Angular Unit Testing!', () => {
   let page: AngularKarmaJasminePage;
 
   beforeEach(() => {
     page = new AngularKarmaJasminePage();
   });
 
-  it('should display welcome message', () => {
+  it('Welcome to Angular Unit Testing!', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Welcome to Angular Unit Testing!');
   });
 });
